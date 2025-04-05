ff6935d6b4d28acad54f3f9aa3e950522242ec8b
Eduardo Cruz <ecc.informatik@icloud.com>
2025-04-05 03:26:48 -0700

    sprint7 - terminado

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Tripleten web_project_around
+
+En este proyecto del sprint 7
+Denominado Al Rededor de Baja California (Alrededor de los E.U.)
+
+se comenzó a crear una pagina Web con las tecnologias HTML CSS y JS
+
+Se esta diseñando una Red Social en donde los usuarios podrán registrarse, escribir acerca de ellos y compartir imagenes de lugares acerca de sus viajes, se iran agregando mas eventos para que los usuarios puedan interactuar más con la página y con otros usuarios.
+
+se implemento un formulario con 2 campos de entrada de datos que capturan el nombre del usuario e información acerca de el, y un boton submit para desplegar en la página la informacion ingresada por el usuario
+
+El formulario se escribio en HTML se le dio Estilos con CSS y se conectaron los campos necesarios atraves de JavaScript
+
+El proyecto contiene:
+
+- una estructura de archivos BEM
+- Metodolgia BEM para la implementacion de clases
+- FlexBox Para la ubicacion de los elementos de acuerdo al Diseño Figma
+- contiene las fuentes indicadas en el diseño
---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const profileName = document.querySelector(".profile-info__name");
+const profileAbout = document.querySelector(".profile-info__about");
+
+const formInputName = document.querySelector(".form__input_name");
+const formInputAbout = document.querySelector(".form__input_about");
+
+const form = document.querySelector(".form");
+
+const profileEditButton = document.querySelector(".profile-info__edit-button");
+const popupProfile = document.querySelector(".popup_profile");
+
+const popupCloseButton = document.querySelector(".popup__close-button");
+
+const cardButtonLikeImage = document.querySelectorAll(
+  ".grid-card__button-image_active"
+);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (formInputName.value !== "" && formInputAbout.value !== "") {
+    profileName.textContent = formInputName.value;
+    profileAbout.textContent = formInputAbout.value;
+    popupProfile.classList.remove("popup_opened");
+  } else {
+    alert("el campo tiene que ser obligatorio");
+  }
+});
+
+profileEditButton.addEventListener("click", function () {
+  popupProfile.classList.add("popup_opened");
+});
+
+popupCloseButton.addEventListener("click", function () {
+  popupProfile.classList.remove("popup_opened");
+});
+
+cardButtonLikeImage.forEach((button) => {
+  button.addEventListener("click", function () {
+    button.classList.toggle("grid-card__button-like-image_active");
+  });
+});
